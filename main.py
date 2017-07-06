f218a84e83a2094e3e935145d7de01356d993976
Jonathan James <jjamesnwa1011@gmail.com>
2017-07-05 20:30:50 -0700

    Added server validation

--- main.py
+++ main.py
@@ -5,17 +5,35 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
-@app.route('/signup')
-def signup():
-
-    return render_template('signup.html')
-
-
-
 
 @app.route('/')
 def index():
-    return redirect('/signup')
+    encoded_error = request.args.get('error')
+    return render_template('signup.html',error=encoded_error)
+
+
+
+@app.route('/adduser', methods=['POST'])
+def add_user():
+    user = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+    email = request.form['email']
+
+    if(not user):
+        error = 'please fill in the username.'
+        return redirect('/?error=' + error)
+    if(password.strip()==""):
+        error = 'Please fill in the password.'
+        return redirect('/?error=' + error)
+    if(verify.strip() != password.strip()):
+        error = 'Passwords do not match. Please try again.'
+        return redirect('/?error=' + error)
+
+
+
+    return render_template('welcome_page.html', username=user)
+
 
 
 
